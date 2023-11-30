8c9f53e46341e03b7ca67c91ec4ca1bd80956fcd
Bilqis Ahmed <bilqis.ali32@gmail.com>
2023-11-30 04:58:09 +0000

    changed all variables from username to userid

--- express_server.js
+++ express_server.js
@@ -32,32 +32,116 @@ const urlDatabase = {
   "b2xVn2": { longURL: "http://www.lighthouselabs.ca" },
   "9sm5xK": { longURL: "http://www.google.com" }
 };
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "123"
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "456",
+  },
+};
 
 //this respons to a get request from the browser when route path is /urls  and directs us the content on the url_index page 
 app.get("/urls", (req, res) => {
+  const userId = req.cookies["user_id"];
+  let user = {
+
+  };
+  let userExists = false; // if user doesnt exsist,
+  for (const id in users) {
+    const dbUser = users[id];
+    if (id === dbUser.id) {
+      user = dbUser;
+    }
+
+  }
   const templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"]
+    user: user
   };
   res.render("urls_index", templateVars);
 });
 // this directs us to the urls/new page where we can implement new urls to be logged/ its content is stored on the url new page 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  const userId = req.cookies["user_id"];
+  let user = {
+
+  };
+  let userExists = false; // if user doesnt exsist,
+  for (const id in users) {
+    const dbUser = users[id];
+    if (id === dbUser.id) {
+      user = dbUser;
+    }
+
+  }
+
+  const templateVars = { user: user };
   res.render("urls_new", templateVars);
+  
 });
 
 
 //Make a new register page 
-app.get('/urls/register', (req, res) => {
-  res.render("urls_register")
-})
-app.post('/urls/register', (req, res) => {
-  const body = req.body
-  const username = body.username;
-  const password = body.password
-  res.render("urls_index")
-})
+app.get('/register', (req, res) => {
+  const userId = req.cookies["user_id"];
+  let user = {
+
+  };
+  let userExists = false; // if user doesnt exsist,
+  for (const id in users) {
+    const dbUser = users[id];
+    if (id === dbUser.id) {
+      user = dbUser;
+    }
+
+  }
+  const templateVars = {
+    user: user
+  };
+  res.render("urls_register", templateVars);
+
+});
+
+//have a place for it to go after it post on the register page
+app.post('/register', (req, res) => {
+  const body = req.body;
+  const email = body.email;
+  const password = body.password;
+
+  //check if user already exsists
+  let userExists = false; // if user doesnt exsist,
+  for (const id in users) {
+    const dbUser = users[id];
+    if (email === dbUser.email) {
+      userExists = true;// if the user already exists we cannot register
+    } else {
+      if (userExists) {
+        res.status(404).end('<p>User already exists!</p>');
+        return;
+      };
+    }
+  }
+
+  const uniqueId = `user${generateRandomString(5)}RandomID`;
+  //add new users to datbase 
+
+  const newUser = {
+    id: uniqueId,
+    email: email,
+    password: password,
+
+  };
+  users[uniqueId] = newUser;
+  res.cookie("user_id", uniqueId);
+  console.log("users", users);
+  //redirect to sign-in page 
+  res.redirect("urls");
+});
 
 // this post the new short url into our database and redirects it to the page we generated the short url for 
 app.post("/urls", (req, res) => {
@@ -83,14 +167,20 @@ app.post("/urls/:id", (req, res) => {
 //Add POST route for /login to expressserver.js
 //Redirect browser back to /urls page after successful login
 app.post("/login", (req, res) => {
-  const username = req.body.username;
-  res.cookie("username", username);
+  const email = req.body.email;
+  res.cookie("user_id", user);
+  //generate random user id
+
+  //set user_id cookie to contain newly genrated 
+
+  //redirect to the url page 
   res.redirect("/urls");
 });
 // made logout button 
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
-  res.redirect("/urls");
+  res.clearCookie("user_id");
+  res.redirect("/register");
+
 });
 //added a sumbit for that redirects to the url page after you enter a long url
 //as long as route paramter is used within route then you can use any variable
@@ -102,7 +192,19 @@ app.get("/u/:id", (req, res) => {
 
 // this takes us to the link generated page for the short url with the corresponding long url 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id].longURL, username: req.cookies["username"] };
+  const userId = req.cookies["user_id"];
+  let user = {
+
+  };
+  let userExists = false; // if user doesnt exsist,
+  for (const id in users) {
+    const dbUser = users[id];
+    if (id === dbUser.id) {
+      user = dbUser;
+    }
+
+  }
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id].longURL, user: user };
   res.render("urls_show", templateVars);
 });
 
