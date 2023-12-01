4f0b893775c8e568419cf9e5ae10e1e24f89a54f
Bilqis Ahmed <bilqis.ali32@gmail.com>
2023-12-01 07:34:01 +0000

    We added a bunch of features

--- express_server.js
+++ express_server.js
@@ -1,7 +1,11 @@
+// things we need to import 
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // which is the default port 
 
+
+// middleware 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 const cookieParser = require('cookie-parser');
@@ -59,6 +63,15 @@ const doesUserExists = (email) => {
     return userExists;
   }
 };
+
+const personLoggedIn = function(cookies) {
+  for (let user in users) {
+    if (user === cookies.user_id) {
+      return true;
+    };
+  }
+  return false;
+};
 //this response to a get request from the browser when route path is /urls  and directs us the content on the url_index page 
 app.get("/urls", (req, res) => {
   const userId = req.cookies["user_id"];
@@ -70,46 +83,78 @@ app.get("/urls", (req, res) => {
 // this directs us to the urls/new page where we can implement new urls to be logged/ its content is stored on the url new page 
 app.get("/urls/new", (req, res) => {
   const userId = req.cookies["user_id"];
-  let user = {
+  let userExists = false;
+  let user = null;
 
-  };
-  let userExists = false; // if user doesnt exsist,
   for (const id in users) {
     const dbUser = users[id];
-    if (id === dbUser.id) {
-      user = dbUser;
+    if (userId === dbUser.id) {
+      userExists = true;
+      user = dbUser
     }
-
+  }
+  if (!userExists) {
+    res.redirect("/login");
+    return
   }
 
-  const templateVars = { user: user };
+const templateVars = {user: user}
   res.render("urls_new", templateVars);
 
 });
 
 ///// added  a login function that has a link at the top of the page.
+
+// Get would be for viewing a page or viewing something
+// Post would be used for login the user into the system - username and password 
+
 app.get('/login', (req, res) => {
 
-  const body = req.body;
-  const email = body.email;
-  const password = body.password;
+  /*
+    check if user already logged in - look at the cookie and compare it with the db
+  
+    1. if user logged in reroute to urls route (urls routes)
+    2. else render urls_login page (login page)
+  */
+  const userId = req.cookies["user_id"];
+  let userExists = false;
+  for (const id in users) {
+    const dbUser = users[id];
+    if (userId === dbUser.id) {
+      userExists = true;
+    }
+  }
 
-  if (email === "" || password === "") {
-    res.status(404).end('<p>Please ensure both email and password are filled in!</p>');
-    return;
-  };
+  if (userExists) {
+    res.redirect("/urls");
+  } else {
+    // show login page
+    res.render("urls_login", { user: null });
+  }
 
-  //check if user already exsists
-  res.render("urls_login", { user: null });
 });
 
 
 //Make a new register page 
 app.get('/register', (req, res) => {
+  const userId = req.cookies["user_id"];
+  let userExists = false;
+  for (const id in users) {
+    const dbUser = users[id];
+    if (userId === dbUser.id) {
+      userExists = true;
+    }
+  }
 
-  res.render("urls_register", { user: null });
-
+  if (userExists) {
+    res.redirect("/urls");
+  } else {
+    res.render("urls_register", { user: null });
+  }
 });
+
+
+
 
 //have a place for it to go after it post on the register page
 app.post('/register', (req, res) => {
@@ -146,6 +191,25 @@ app.post('/register', (req, res) => {
 
 // this post the new short url into our database and redirects it to the page we generated the short url for 
 app.post("/urls", (req, res) => {
+
+
+  //checking is user is logged in
+  const userId = req.cookies["user_id"];
+  let userExists = false;
+  for (const id in users) {
+    const dbUser = users[id];
+    if (userId === dbUser.id) {
+      userExists = true;
+    }
+  }
+// if user is not logged in tell them they need to log in 
+  if (!userExists) {
+    res.status(403).end('<p>You need to be logged in to shorten urls!</p>');
+        return; 
+  } 
+
+
+  //create shorten urls /////////////////////////////////
   const longURL = req.body.longURL;
   const shortURL = generateRandomString(5);
 
@@ -164,6 +228,7 @@ app.post("/urls/:id", (req, res) => {
 
   urlDatabase[id].longURL = newLongURL;
   res.redirect("/urls");
+  
 });
 
 
@@ -175,7 +240,7 @@ app.post("/login", (req, res) => {
   const body = req.body;
   const email = body.email;
   const password = String(body.password);
-
+  let userId = null;
   //check if user already exsists
   if (!doesUserExists(email)) {
     res.status(403).end('<p>Register new account</p>');
@@ -189,40 +254,51 @@ app.post("/login", (req, res) => {
       if (password !== dbUser.password) {
         res.status(403).end('<p>Password is not the same</p>');
         return;
-      } 
-
-        console.log("dbUser[id]", dbUser["id"], "dbUser.id", dbUser.id);
-        res.cookie("user_id", dbUser.id);
-
+      }
+      console.log("dbUser[id]", dbUser["id"], "dbUser.id", dbUser.id);
+      userId = dbUser.id;
     }
+
     
+
   }
-  
-  
+
   //1) we register new users
   //2) we log the new user in the database with cookies
   //3) sign in with the email we've registered 
-  
-  
-  
-  
+
+  res.cookie("user_id", userId);
   res.redirect("/urls");
-  
-  
+
 });
+
+
+
+
+
 // made logout button 
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
   res.redirect("/login");
 });
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //added a sumbit for that redirects to the url page after you enter a long url
 //as long as route paramter is used within route then you can use any variable
 app.get("/u/:id", (req, res) => {
-  const shortURL = req.params.id;
-  const longURL = urlDatabase[shortURL].longURL;
-  res.redirect(longURL);
+  const shortID = req.params.id;
+  const longURL = urlDatabase[shortID].longURL;
+  if (!longURL) {
+    res.status(404).send("Page Not Found");
+    return;
+  }
+  res.redirect(longURL)
 });
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // this takes us to the link generated page for the short url with the corresponding long url 
 app.get("/urls/:id", (req, res) => {
   const userId = req.cookies["user_id"];
@@ -274,24 +350,3 @@ app.post("/urls/:id/delete", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-
-
-
-
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-
-// });
-// app.get("/set", (req, res) => {
-//   const a = 1;
-//   res.send(`a = ${a}`);
-// });
-
-// app.get("/fetch", (req, res) => {
-//   res.send(`a = ${a}`);
-// // });
-// app.get("/ali", (req, res) => {
-//   res.send("Hello Ali Maydhane !");
-// });
